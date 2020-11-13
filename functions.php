2b407e36e378a9c2a2dc918c77849d5935f78d05
Emma <58231531+ECWvN@users.noreply.github.com>
2020-11-13 15:29:56 +0100

    hihi

--- functions.php
+++ functions.php
@@ -41,7 +41,7 @@ function register(){
 	} 
 
 
-// kijken of de email of gebruikersnaam al bestaat
+
   $user_check_query = "SELECT * FROM users WHERE username='$username' OR email='$email' LIMIT 1";
   $result = mysqli_query($db, $user_check_query);
   $user = mysqli_fetch_assoc($result);
